30a8274078eac1278c08b8cf62b6d950f5f7d01f
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-06-18 16:06:33 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,16 @@
+a = 10
+b = 20
+
+print("Before Swapping")
+print("a = ", a)
+print("b = ", b)
+
+# c = a
+# a = b
+# b = c
+
+a,b = b,a
+
+print("after Swapping")
+print("a = ", a)
+print("b = ", b)
