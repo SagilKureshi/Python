10eaef5ec89320a3dcd40bf0efd4fd83762c4c5f
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-06-18 21:53:35 -0700

    add more ways to swap

--- swap.py
+++ swap.py
@@ -1,16 +1,21 @@
 a = 10
-b = 20
+b = 20 
+print("before swaping")
+print("a = ",a,"\nb = ",b)
 
-print("Before Swapping")
-print("a = ", a)
-print("b = ", b)
-
+#1st way
 # c = a
 # a = b
 # b = c
 
-a,b = b,a
+#2nd way
 
-print("after Swapping")
-print("a = ", a)
-print("b = ", b)
+# a = a + b # a = 10+20 = a= 30
+# b = a - b # b = 30-20 = b= 10
+# a = a - b # a = 30-10 = a= 20 
+
+#3rd way
+a,b = b,a
+print("after swaping")
+print("a = ",a,"\nb = ",b)
+
