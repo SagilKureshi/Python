d6e9f207d44105d74d740d3a21dd98faaf3b8690
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-06-21 23:31:33 -0700

    Add files via upload

---
+++ Numeric_Datatype.py
@@ -0,0 +1,9 @@
+a = 20
+b = 5.5
+name = "Sagil"
+c  = 1j
+
+print(type(a))
+print(type(b))
+print(type(name))
+print(type(c))
---
+++ bool1.py
@@ -0,0 +1,7 @@
+a = 200
+b = 30
+
+if a>b:
+    print("A is greater than B")
+else: 
+    print("B Greater than A")
