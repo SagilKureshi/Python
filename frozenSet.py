783b8113ff9231b4cdcf35b0b8b20bc92f711f55
Sagil Kureshi <sagilkureshi007@gmail.com>
2026-07-03 18:28:13 +0530

    add set.py and frozenset.py

---
+++ frozenSet.py
@@ -0,0 +1,6 @@
+a = {1,2,3,4,5}
+fz = frozenset(a)
+print(fz)
+
+#fz.update(5) unchangeable
+#fz.remove(1)
---
+++ set.py
@@ -0,0 +1,13 @@
+a = {1,2,3,4,5}
+print("set datatype:- ")
+print(a)
+
+print("print using loop :- ")
+for i in a:
+    print(i,end=" ")
+
+ch = set("hello")
+print("\n",ch)
+
+a.update([6,7])
+print(a)
