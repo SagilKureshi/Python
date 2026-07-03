40f85349213d3595d0029943f13b8169309d789a
Sagil Kureshi <sagilkureshi007@gmail.com>
2026-07-03 18:43:39 +0530

    add dictionary_1 and dictionary_2

---
+++ dictionary_1.py
@@ -0,0 +1,7 @@
+dics = {1 : "anil", 2 : "bablu", 3: "carry"}
+print(dics)
+
+print(dics[1])
+print(dics[3])
+print(dics.keys())
+print(dics.values())
---
+++ dictionary_2.py
@@ -0,0 +1,15 @@
+student = {
+    "name" : "sagil",
+    "surname" : "kureshi",
+    "course" : "BCA" 
+}
+print(student)
+print(student["name"])
+print(student["course"])
+print(student.keys())
+print(student.values())
+print(len(student))
+print(type(student))
+
+student = dict(name = "harry" , age = "21")
+print(student)
