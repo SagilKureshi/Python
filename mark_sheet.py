61cfb22593bc9d5dc4bffcb4d1ddf12f6c8e9cea
SagilKureshi <sagilkureshi007@gmail.com>
2026-07-04 00:13:57 -0700

    add marksheet

---
+++ mark_sheet.py
@@ -0,0 +1,18 @@
+roll = 101
+print("roll no = ",roll)
+
+name = "xyz"
+print("name = ",name)
+
+
+sub1 = 50
+sub2 = 50
+sub3 = 50
+
+total = sub1+sub2+sub3
+
+print("Total : ",total)
+
+per = total/3
+
+print(per)
