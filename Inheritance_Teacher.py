bb35d97f889e5fc6c8c26785ca5bca1061c59277
Sagil-Kureshi <sagilkureshi007@gmail.com>
2026-08-05 11:49:22 +0530

    Add files via upload

---
+++ Inheritance_Teacher.py
@@ -0,0 +1,13 @@
+from teacher import *
+
+t = Teacher()
+
+t.setId(101)
+t.setName("sagil")
+t.setAddress("Amreli")
+t.setSalary(50000.00)
+
+print("id : ",t.id)
+print("name : ",t.name)
+print("address :",t.address)
+print("salary : ",t.salary)
---
+++ teacher.py
@@ -0,0 +1,30 @@
+class Teacher:
+
+    def setId(self,id):
+        self.id = id
+    
+    def getId(self):
+        return self.id
+
+    def setName(self,name):
+        self.name = name
+
+    def getName(self):
+        return self.name
+
+    def setAddress(self,address):
+        self.address =address
+
+    def getAddress(self):
+        return self.address
+
+    def setSalary(self,salary):
+        self.salary = salary
+
+    def getSalary(self):
+        return self.salary
+
+
+
+ 
+    
